d8b6ea956186ec9b34d93e16977214435863ba6a
NelsonIg <nelson.igbokwe@web.de>
2021-01-23 19:39:10 +0100

    publish temp and humid

--- esp8266/mqtt_esp8266/mqtt_esp8266.ino
+++ esp8266/mqtt_esp8266/mqtt_esp8266.ino
@@ -1,38 +1,33 @@
 /*
  Basic ESP8266 MQTT example
- This sketch demonstrates the capabilities of the pubsub library in combination
- with the ESP8266 board/library.
- It connects to an MQTT server then:
-  - publishes "hello world" to the topic "outTopic" every two seconds
-  - subscribes to the topic "inTopic", printing out any messages
-    it receives. NB - it assumes the received payloads are strings not binary
-  - If the first character of the topic "inTopic" is an 1, switch ON the ESP Led,
-    else switch it off
- It will reconnect to the server if the connection is lost using a blocking
- reconnect function. See the 'mqtt_reconnect_nonblocking' example for how to
- achieve the same result without blocking the main loop.
- To install the ESP8266 board, (using Arduino 1.6.4+):
-  - Add the following 3rd party board manager under "File -> Preferences -> Additional Boards Manager URLs":
-       http://arduino.esp8266.com/stable/package_esp8266com_index.json
-  - Open the "Tools -> Board -> Board Manager" and click install for the ESP8266"
-  - Select your ESP8266 in "Tools -> Board"
+
+ * Pins for ESP82666  LCD1601 (0x27) BMP280 (0x76)
+ *            Vin       VCC           VIN
+ *            GND       GND           GND
+ *            D2        SDA           SDA
+ *            D1        SCL           SCL
 */
+
 
 
 // test with mosquitto_pub -t LED -m [0/1]
 
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME280.h>
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 #include "secrets.h"
+#include <stdio.h>
 
 // Update these with values suitable for your network.
-
 const char* ssid = STASSID;
 const char* password = STAPSK;
 const char* mqtt_server = "192.168.0.183";
 
 WiFiClient espClient;
 PubSubClient client(espClient);
+Adafruit_BME280 bme; // I2C
+
 unsigned long lastMsg = 0;
 #define MSG_BUFFER_SIZE	(50)
 char msg[MSG_BUFFER_SIZE];
@@ -82,8 +77,8 @@ void callback(char* topic, byte* payload, unsigned int length) {
 
 }
 
-void reconnect() {
-  // Loop until we're reconnected
+void reconnect_sub() {
+  // Loop until we're reconnected and subscribe to topic
   while (!client.connected()) {
     Serial.print("Attempting MQTT connection...");
     // Create a random client ID
@@ -103,12 +98,65 @@ void reconnect() {
   }
 }
 
+void reconnect(){
+  // Loop until connected and publish to topic
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Create a random client ID
+    String clientId = "ESP8266Client-";
+    clientId += String(random(0xffff), HEX);
+    // Attempt to connect
+    if (client.connect(clientId.c_str())) {
+      Serial.println("connected");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+void sensorInit(){
+  unsigned status;
+  status = bme.begin(0x76);
+    if (!status) {
+        Serial.println("Could not find a valid BME280 sensor, check wiring, address, sensor ID!");
+        Serial.print("SensorID was: 0x"); Serial.println(bme.sensorID(),16);
+        Serial.print("        ID of 0xFF probably means a bad address, a BMP 180 or BMP 085\n");
+        Serial.print("   ID of 0x56-0x58 represents a BMP 280,\n");
+        Serial.print("        ID of 0x60 represents a BME 280.\n");
+        Serial.print("        ID of 0x61 represents a BME 680.\n");
+        while (1) delay(10);
+    }
+    
+    Serial.println("-- Default Test --");
+    Serial.println();
+}
+
 void setup() {
+  sensorInit();
   pinMode(BUILTIN_LED, OUTPUT);     // Initialize the BUILTIN_LED pin as an output
   Serial.begin(115200);
   setup_wifi();
   client.setServer(mqtt_server, 1883);
   client.setCallback(callback);
+}
+
+void sensorPub(){
+  // Publish sensor data
+  // test with: mosquitto_sub -t "bme280"
+  String temp = String(bme.readTemperature());
+  String hum = String(bme.readHumidity());
+  // create json msg
+  String json = "{\"temperature\":" + temp + ",\"humidity\":"+ hum + "}";
+  
+  char charMsg [json.length()-1];
+  for (int i=0; i<json.length(); i++){
+    charMsg[i]=json[i];
+  }
+  client.publish("bme280", charMsg);
 }
 
 void loop() {
@@ -117,4 +165,6 @@ void loop() {
     reconnect();
   }
   client.loop();
+  sensorPub();
+  delay(5000);
 }
