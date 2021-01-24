164e4e348d667a3cdcdfcdac931d722abf6accaf
NelsonIg <nelson.igbokwe@web.de>
2021-01-24 22:56:30 +0100

    random test

--- python/plot_data.py
+++ python/plot_data.py
@@ -7,8 +7,8 @@ now = date.datetime.now()
 sensor_data = pd.read_csv(f'{now.year}-{now.day}-sensor_data.csv')
 
 # compute average
-WINDOW_SIZE = 5
-WINDOW_TYPE = None  # 'gaussian, 'triang'
+WINDOW_SIZE = 6
+WINDOW_TYPE = 'gaussian'  # None, 'triang'
 moving_average = sensor_data.rolling(WINDOW_SIZE).mean()
 STEP = 1
 data_slice = moving_average[::STEP]
@@ -16,6 +16,6 @@ data_slice = moving_average[::STEP]
 fix, axis = plt.subplots(1,1)
 ax = data_slice.plot(y=['temperature', 'humidity'], label=["temperature mean [°C]", "humidity mean [%]"], ax=axis)
 ax2 = sensor_data.plot(y=['temperature', 'humidity'], label=["temperature [°C]", "humidity [%]"], ax=axis)
-plt.title('Big Window half opened')
+plt.title('Random Test')
 plt.xlabel(f'Window: ({WINDOW_SIZE}, {WINDOW_TYPE})')
 plt.show()
