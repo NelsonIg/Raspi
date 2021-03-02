8a863a56ad9304ce084103e883dac4a501f9b025
NelsonIg <nelson.igbokwe@web.de>
2021-03-02 15:46:34 +0100

    commit

--- OPC_UA/python/server/server_sub.py
+++ OPC_UA/python/server/server_sub.py
@@ -64,7 +64,7 @@ async def get_rpm(rpm_var):
     '''
     read round per minute periodically
     '''
-    await asyncio.sleep(0.05)
+    await asyncio.sleep(0.025)
     rpm_is = await rpm()
     if rpm_is>-1:
         await rpm_var.write_value(rpm_is)
