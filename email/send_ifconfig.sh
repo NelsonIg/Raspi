451bbf61d5a6b731945f3d5990fc94204a04f914
NelsonIg <nelson.igbokwe@web.de>
2021-06-21 16:52:27 +0200

    deleted enviroment variables

--- email/send_ifconfig.sh
+++ email/send_ifconfig.sh
@@ -2,4 +2,4 @@
 
 sleep 1m # sleep 1 minute so to ensure internet connection
 MSG=$(ifconfig)
-python3 send_email.py -a $EMAIL_ADDR -p $EMAIL_PWD -m "$MSG"
+python3 send_email.py -a <address> -p <password> -m "$MSG"
